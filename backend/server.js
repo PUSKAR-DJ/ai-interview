dca75e593dee6aa9f2081aef1c1a32c4a8235e8c
PUSKAR DJ <puskarsaha3@gmail.com>
2026-01-01 14:27:40 +0530

    Revert "test backend failure ~6"
    This reverts commit a88fdea86480aedf56dcf0dd7d85dba56b84dca9.

--- backend/server.js
+++ backend/server.js
@@ -16,17 +16,10 @@ const PORT = process.env.PORT || 3045;
 connectDB();
 
 // Middleware
-// app.use(cors({
-//   origin: process.env.FRONTEND_URL || "http://localhost:5173",
-//   credentials: true
-// }));
-
 app.use(cors({
-  origin: true,
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true
 }));
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,26 +1,16 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_BASE_URL,
-//   withCredentials: true, // IMPORTANT if cookies are used
-// });
-
-// api.interceptors.response.use(
-//   (res) => res,
-//   (err) => {
-//     // global error handling later
-//     return Promise.reject(err);
-//   }
-// );
-
-// export default api;
-
-// src/api/axios.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
-  withCredentials: true
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true, // IMPORTANT if cookies are used
 });
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    // global error handling later
+    return Promise.reject(err);
+  }
+);
 
 export default api;
