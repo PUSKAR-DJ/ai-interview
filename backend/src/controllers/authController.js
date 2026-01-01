52159cc712456c12623f57d0e0710a6cfb7d5bf2
PUSKAR DJ <puskarsaha3@gmail.com>
2026-01-01 12:49:54 +0530

    testing backend failure ~1

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -50,6 +50,10 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  res.clearCookie("token");
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None"
+  });
   return res.json({ message: "Logged out" });
 };
