a88fdea86480aedf56dcf0dd7d85dba56b84dca9
PUSKAR DJ <puskarsaha3@gmail.com>
2026-01-01 14:13:29 +0530

    test backend failure ~6

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,17 @@ const PORT = process.env.PORT || 3045;
 connectDB();
 
 // Middleware
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL || "http://localhost:5173",
+//   credentials: true
+// }));
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: true,
   credentials: true
 }));
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
